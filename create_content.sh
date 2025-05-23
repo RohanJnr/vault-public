289ab9f514651f6ec25b22447970efa9fe502c2c
RohanReddy44 <alletirohan@sahaj.ai>
2025-05-23 11:21:17 +0530

    update script

--- create_content.sh
+++ create_content.sh
@@ -1,6 +1,7 @@
 rm -rf content
 mkdir content
-cp -r ./obsidian-backup/Assets/Images ./content/Assets
+mkdir content/Assets
+cp -r ./obsidian-backup/Assets/Images ./content/Assets/Images
 cp -r ./obsidian-backup/Notes ./content/
 cp -r ./obsidian-backup/index.md ./content/
 cp -r ./obsidian-backup/Home\ pages/* ./content/
