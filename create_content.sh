7ef947665c179b2d483210cab3b0b848c178a2d8
RohanReddy44 <alletirohan@sahaj.ai>
2025-05-23 11:14:42 +0530

    only copy images

--- create_content.sh
+++ create_content.sh
@@ -1,6 +1,6 @@
 rm -rf content
 mkdir content
-cp -r ./obsidian-backup/Assets ./content/
+cp -r ./obsidian-backup/Assets/Images ./content/Assets
 cp -r ./obsidian-backup/Notes ./content/
 cp -r ./obsidian-backup/index.md ./content/
 cp -r ./obsidian-backup/Home\ pages/* ./content/
