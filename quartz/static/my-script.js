e407ce82f151ef6e5f6f82799675fe419f970cae
RohanReddy44 <alletirohan@sahaj.ai>
2025-11-03 10:42:28 +0530

    Testing matomo

--- quartz/static/my-script.js
+++ quartz/static/my-script.js
@@ -1,1 +1,14 @@
 window.reflectConfig = { key: "widget_529575f39e33463f", position: "bottom_right" };
+
+
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function () {
+    var u = "https://rohanreddyalleti.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.async = true; g.src = 'https://cdn.matomo.cloud/rohanreddyalleti.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g, s);
+})();
